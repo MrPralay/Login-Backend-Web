78f444e95e037195f9bcb471cf358e837c40e9cc
MrPralay <pralayd140@gmail.com>
2026-01-15 19:31:51 +0530

    next

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,14 +9,17 @@ const OTP = require('../models/OTP');
 
 // Configure Nodemailer
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com', 
+    port: 465,            
+    secure: true,         
     auth: {
         type: 'OAuth2',
         user: process.env.EMAIL_USER,
         clientId: process.env.GMAIL_CLIENT_ID,
         clientSecret: process.env.GMAIL_CLIENT_SECRET,
         refreshToken: process.env.GMAIL_REFRESH_TOKEN
-    }
+    },
+    connectionTimeout: 10000 // 10 seconds timeout
 });
 
 // Verify Connection Configuration
