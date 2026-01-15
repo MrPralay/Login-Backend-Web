953413132f2893cd2bd774b3320590a9bbb294c8
MrPralay <pralayd140@gmail.com>
2026-01-15 18:11:16 +0530

    last

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,8 +10,8 @@ const OTP = require('../models/OTP');
 // Configure Nodemailer
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false, // Use STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
